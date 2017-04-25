b5f1097f13092b8eff7e011f4dca1ccb25abf77e
nekomeo <elleti@Elles-MacBook-Air.local>
2017-04-25 16:30:49 -0700

    Initial Commit

---
+++ Assignment 2: Two Doors/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Assignment 2: Two Doors
+//
+//  Created by Elle Ti on 2017-04-25.
+//  Copyright © 2017 Elle Ti. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
